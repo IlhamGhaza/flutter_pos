5465987ddd861f5283ad8ee4a7adfdcd48ed471d
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-09-27 20:52:18 +0700

    feat: adds new components for UI widgets

---
+++ lib/core/constants/variables copy.dart
@@ -0,0 +1,5 @@
+class Variables {
+  static const String baseUrl =
+      'http://[your ip]:8000'; //or replace with your website url as backend
+  static const String imageBaseUrl = '$baseUrl/storage/products/';
+}
---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_pos/core/assets/assets.gen.dart';
+import 'package:flutter_pos/data/datasources/auth_local_datasource.dart';
+import 'package:flutter_pos/data/datasources/auth_remote_datasource.dart';
+import 'package:flutter_pos/data/datasources/midtrans_remote_datasource.dart';
+import 'package:flutter_pos/data/datasources/order_remote_datasource.dart';
+import 'package:flutter_pos/data/datasources/product_local_datasource.dart';
+import 'package:flutter_pos/data/datasources/product_remote_datasource.dart';
+import 'package:flutter_pos/data/datasources/report_remote_datasource.dart';
+import 'package:flutter_pos/presentation/auth/pages/login_page.dart';
+import 'package:flutter_pos/presentation/draft_order/bloc/draft_order/draft_order_bloc.dart';
+import 'package:flutter_pos/presentation/history/bloc/history/history_bloc.dart';
+import 'package:flutter_pos/presentation/home/bloc/category/category_bloc.dart';
+import 'package:flutter_pos/presentation/home/bloc/checkout/checkout_bloc.dart';
+import 'package:flutter_pos/presentation/home/bloc/product/product_bloc.dart';
+import 'package:flutter_pos/presentation/home/pages/dashboard_page.dart';
+import 'package:flutter_pos/presentation/order/bloc/order/order_bloc.dart';
+import 'package:flutter_pos/presentation/order/bloc/qris/qris_bloc.dart';
+import 'package:flutter_pos/presentation/setting/bloc/report/close_cashier/close_cashier_bloc.dart';
+import 'package:flutter_pos/presentation/setting/bloc/report/product_sales/product_sales_bloc.dart';
+import 'package:flutter_pos/presentation/setting/bloc/report/summary/summary_bloc.dart';
+import 'package:flutter_pos/presentation/setting/bloc/sync_order/sync_order_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'core/constants/colors.dart';
+import 'presentation/auth/bloc/login/login_bloc.dart';
+import 'presentation/home/bloc/logout/logout_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => LoginBloc(AuthRemoteDatasource()),
+        ),
+        BlocProvider(
+          create: (context) => LogoutBloc(AuthRemoteDatasource()),
+        ),
+        BlocProvider(
+          create: (context) => ProductBloc(ProductRemoteDatasource())
+            ..add(const ProductEvent.fetchLocal()),
+        ),
+        BlocProvider(create: (context) => CheckoutBloc()),
+        BlocProvider(
+          create: (context) => OrderBloc(),
+        ),
+        BlocProvider(
+          create: (context) => QrisBloc(MidtransRemoteDatasource()),
+        ),
+        BlocProvider(
+          create: (context) => HistoryBloc(),
+        ),
+        BlocProvider(
+          create: (context) => SyncOrderBloc(OrderRemoteDatasource()),
+        ),
+        BlocProvider(
+          create: (context) => CategoryBloc(ProductRemoteDatasource()),
+        ),
+        BlocProvider(
+          create: (context) => DraftOrderBloc(ProductLocalDatasource.instance),
+        ),
+        BlocProvider(
+          create: (context) => SummaryBloc(ReportRemoteDatasource()),
+        ),
+        BlocProvider(
+          create: (context) => ProductSalesBloc(ReportRemoteDatasource()),
+        ),
+        BlocProvider(
+          create: (context) => CloseCashierBloc(ReportRemoteDatasource()),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'POS App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+          useMaterial3: true,
+          textTheme: GoogleFonts.quicksandTextTheme(
+            Theme.of(context).textTheme,
+          ),
+          appBarTheme: AppBarTheme(
+            color: AppColors.white,
+            elevation: 0,
+            titleTextStyle: GoogleFonts.quicksand(
+              color: AppColors.primary,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w500,
+            ),
+            iconTheme: const IconThemeData(
+              color: AppColors.primary,
+            ),
+          ),
+        ),
+        home: FutureBuilder<bool>(
+            future: AuthLocalDatasource().isAuth(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData && snapshot.data == true) {
+                return const DashboardPage();
+              } else {
+                return const LoginPage();
+              }
+            }),
+      ),
+    );
+  }
+}
