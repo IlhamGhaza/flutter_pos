a514fbb53a9e0edea387758d0e3fdc06d48f4a30
Ilham Ghazali <m.ilhamghazali@gmail.com>
2025-09-25 10:40:50 +0700

    feat : change localization

--- lib/core/constants/variables.dart
+++ lib/core/constants/variables.dart
@@ -1,6 +1,6 @@
 class Variables {
   static const String baseUrl =
       // 'http://192.168.1.11:8000'; //or replace with your website url as backend
-      'https://myposs.ilhamghazali.my.id';
+      '`';
   static const String imageBaseUrl = '$baseUrl/storage/products/';
 }
